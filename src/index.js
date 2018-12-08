1d9a9f3d867bbd80b53d001f5272c8c8196c0adb
Alex Vechkanov <hydrock74@gmail.com>
2018-12-09 00:39:05 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -6,42 +6,83 @@ var headerElement = document.querySelector('header');
 sectionElement.style.left = '100px';
 sectionElement.style.top = '100px';
 
+var layerX, layerY;
 
 // элемент перетаскивается или нет (в данный момент)
 var draggable = false;
 
-var layerX, layerY;
-// Зажимаем мышь на заголовке
-headerElement.addEventListener('mousedown', function(e) {
+function startDrag (type, e) {
     // говорим о том что окно перетаскивается
     draggable = true;
 
-    // layerX,layerY - это позиция мыши относительно таргета (не окна браузера)
-    layerX = e.layerX;
-    layerY = e.layerY;
+    layerX = 0;
+    layerY = 0;
 
     // Добавляем обработчик на движение
-    document.addEventListener('mousemove', setWindowPosition);
-})
+    if (type === 'click') {
+        // layerX,layerY - это позиция мыши относительно таргета (не окна браузера)
+        layerX = e.layerX;
+        layerY = e.layerY;
+        document.addEventListener('mousemove', setWindowPosition);
+    } else if (type === 'touch') {
+        layerX = parseInt((e.touches[0].clientX - e.target.parentNode.offsetLeft), 10);
+        layerY = parseInt((e.touches[0].clientY - e.target.parentNode.offsetTop), 10);
+        document.addEventListener('touchmove', setWindowPosition);
+    }
+}
 
-var newClientX, newClientY;
-document.addEventListener('mouseup', function() {
+function stopDrag () {
     // выключаем перетаскивание
     draggable = false;
     document.removeEventListener('mousemove', setWindowPosition);
+    document.removeEventListener('touchmove', setWindowPosition);
+}
+
+// Зажимаем мышь на заголовке
+headerElement.addEventListener('mousedown', function(e) {
+    startDrag('click', e);
+});
+
+// Касаемся заголовка
+headerElement.addEventListener('touchstart', function(e) {
+    startDrag('touch', e);
+});
+
+document.addEventListener('mouseup', function() {
+    stopDrag();
+});
+
+document.addEventListener('touchend', function() {
+    stopDrag();
 });
 
 function setWindowPosition(e) {
-    // e.buttons - какие кнопки мыши нажаты - 1 это левая
-    // если нажата левая кнопка мыши и элемент перетаскивается
-    if (e.buttons === 1 && draggable) {
-        // вычисляем стили для окна
-        // берем текущее положение мыши от окна браузера
-        // и отнимаем то расстояние от хеадера до мыши
-        // в итоге окно всегда будет иметь позицию относительно
-        // со смещением
-        sectionElement.style.left = (e.clientX - layerX) + 'px';
-        sectionElement.style.top = (e.clientY - layerY) + 'px';
+    if (!draggable) return;
+
+    //console.log(e.touches[0]);
+
+    if (e.type === 'mousemove') {
+        // e.buttons - какие кнопки мыши нажаты - 1 это левая
+        // если нажата левая кнопка мыши и элемент перетаскивается
+        if (e.buttons === 1) {
+            // вычисляем стили для окна
+            // берем текущее положение мыши от окна браузера
+            // и отнимаем то расстояние от хеадера до мыши
+            // в итоге окно всегда будет иметь позицию относительно
+            // со смещением
+            sectionElement.style.left = (e.clientX - layerX) + 'px';
+            sectionElement.style.top = (e.clientY - layerY) + 'px';
+        }
+    } else if (e.type === 'touchmove') {
+        // console.log(
+        //         parseInt(e.touches[0].clientX, 10),
+        //         parseInt(e.touches[0].clientY, 10)
+        //     );
+        console.log(layerY)
+        sectionElement.style.left =
+            parseInt((e.touches[0].clientX - layerX), 10) + 'px';
+        sectionElement.style.top =
+            parseInt((e.touches[0].clientY - layerY), 10) + 'px';
     }
 }
 
